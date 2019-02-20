c5b49eed9d2a166c6be5a5289d85b8fba48b2e5e
Anmol Rishi <anmol1rishi@gmail.com>
2019-02-20 21:34:15 +0530

    Added initial Techweek page

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('#sponsors-list').owlCarousel({
+        items:1,
+        autoplay:true,
+        smartSpeed:700,
+        loop:true,
+        autoPlayHoverPause:true,
+        nav:true,
+        dots:false,
+        navText:['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>' ]
+    })
+});
